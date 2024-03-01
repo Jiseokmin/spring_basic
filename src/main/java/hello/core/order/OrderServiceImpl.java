105553af8faeedd3d4b544ccadb1c04cdbc1d933
jiseokmin <37jr@naver.com>
2024-03-01 15:57:17 +0900

    add spring

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,9 +1,9 @@
 package hello.core.order;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
